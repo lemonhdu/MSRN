c25635229c0acbe59174f7fe6166053f3f97f13c
lemonhdu <813077173@qq.com>
2022-12-19 20:44:53 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -31,28 +31,36 @@ def get_time_points(predicted):
     predicted_length = len(predicted[0])
 
     state_index = 'idle'
-
+    
+    num_points = 0
     for i in range(predicted_length):
 
-        if predicted[0][i] == 1 and predicted[0][i+1] == 2 and state_index == 'idle':
+        if predicted[0][i] == 0 and predicted[0][i + 1] == 1 and state_index == 'idle':
+            num_points += 1
+            state_index = 'jump'
+            
+        elif predicted[0][i] == 1 and predicted[0][i+1] == 2 and (state_index == 'jump' or 'idle'):
             points.append(i+1)
+            num_points += 1
             state_index = 'drop'
 
         elif predicted[0][i] == 2 and predicted[0][i+1] == 3 and state_index == 'drop':
             points.append(i+1)
+            num_points += 1
             state_index = 'entering'
 
         elif predicted[0][i] == 3 and predicted[0][i+1] == 4 and state_index == 'entering':
             points.append(i+1)
+            num_points += 1
             state_index = 'ending'
             break
 
     if points == []:
-        return None
+        return None, None
 
     else:
-        points = np.array(points).reshape(1, 3)
-        return points
+        points = np.array(points).reshape(1, -1)
+        return points, num_points
 
 
 def trans_time_labels(time_labels):
@@ -74,15 +82,15 @@ def trans_time_labels(time_labels):
     return points
 
 
-def get_stage_features(i3d_feature, tcn_predict_time_points, time_point_label_length):
+def get_stage_features(i3d_feature, tcn_predict_time_points, time_point_num):
 
     i3d_feature = i3d_feature.squeeze(0).transpose(1, 0)
 
-    if time_point_label_length == 3:
+    if time_point_num == 3:
         # [jumping, dropping point]
         i3d_feature_jumping = i3d_feature[:tcn_predict_time_points[0][0]]
 
-    elif time_point_label_length == 4:
+    elif time_point_num == 4:
         min_index = tcn_predict_time_points[0][0]-5 if tcn_predict_time_points[0][0]-5 >= 0 else 0
         # [dropping point - 5, dropping point]
         i3d_feature_jumping = i3d_feature[min_index:tcn_predict_time_points[0][0]]
